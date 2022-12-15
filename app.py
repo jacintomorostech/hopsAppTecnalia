0539aed68c5bd527a8c67dfc23d2f68af1bb03d1
Jacinto Jesús Moros Montañés <jesus.moros@tecnalia.com>
2022-12-15 19:15:37 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import ghhops_server as hs
+import rhino3dm
+
+
+# register hops app as middleware
+app = Flask(__name__)
+hops: hs.HopsFlask = hs.Hops(app)
+
+
+# flask app can be used for other stuff drectly
+@app.route("/help")
+def help():
+    return "Welcome to Grashopper Hops for CPython!"
+
+
+@hops.component(
+    "/binmult",
+    inputs=[hs.HopsNumber("A"), hs.HopsNumber("B")],
+    outputs=[hs.HopsNumber("Multiply")],
+)
+def BinaryMultiply(a: float, b: float):
+    return a * b
+
+if __name__ == "__main__":
+    app.run(debug=True)
